2adee1c03ac7723e911ade25499eade0d899b6f8
Alexander <fortuna12_12@mail.ru>
2025-01-15 22:08:23 +0700

    Add all tables to __init__ for easier imports

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from .user import User
+from .flower import Flower
+from .personal_flower import PersonalFlower
+from .advice import Advice
+from .advice_photo import AdvicePhoto
+from .message import Message
+from .note import Note
+from .type_note import TypeNote
+from .review import Review
+from .review_photo import ReviewPhoto
+
