9626ebcdb4555f46c4bf2341574c235c44444feb
Alexander <fortuna12_12@mail.ru>
2025-01-15 22:07:49 +0700

    Remove Mapped from relationship and fix cyclic imports between tables

--- app/models/advice_photo.py
+++ app/models/advice_photo.py
@@ -2,7 +2,6 @@ from app.core.database import Base
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.models.advice import Advice
 
 
 class AdvicePhoto(Base):
@@ -12,4 +11,4 @@ class AdvicePhoto(Base):
     advice_id: Mapped[int] = mapped_column(ForeignKey("advices.id"), nullable=False)
     photo: Mapped[str] = mapped_column(String(150), nullable=False)
 
-    advice: Mapped["Advice"] = relationship("Advice", back_populates="advice_photos")
+    advice = relationship("Advice", back_populates="advice_photos")
