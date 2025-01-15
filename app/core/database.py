f7700d48b38660962fec110ac57460df88d328fd
Alexander <fortuna12_12@mail.ru>
2025-01-15 22:10:06 +0700

    Added DATABASE_URL from config for easier import in Alembic

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker,AsyncSession, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped
-from config import settings
+from app.core.config import settings
 from app.utils.base import created_at, updated_at, int_pk
 
 engine = create_async_engine(settings.DATABASE_URL, future=True, echo=True)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False, )
+DATABASE_URL = settings.DATABASE_URL
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
