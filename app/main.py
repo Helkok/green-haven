3e3aa3438a9dd7c62d9136b4e4d81f4ddcadc9f6
Alexander <fortuna12_12@mail.ru>
2025-01-26 21:44:44 +0700

    Include auth and flower routers (test protected)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from app.middleware.middleware import DBSessionMiddleware
+from app.routers import auth_route
+from app.routers import flower_route
+from app.utils.user import get_current_user
 
 app = FastAPI()
 app.add_middleware(DBSessionMiddleware)
@@ -9,3 +12,14 @@ app.add_middleware(DBSessionMiddleware)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/protected")
+async def protected_route(current_user_id: str = Depends(get_current_user)):
+    return {"msg": "Access granted", "user_id": current_user_id}
+
+
+PROTECTED = [Depends(get_current_user)]
+
+app.include_router(auth_route, tags=["auth"], prefix="/auth")
+app.include_router(flower_route, tags=["flowers"], prefix="/flowers")
