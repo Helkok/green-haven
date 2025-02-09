1d5aef88b8c2a3b5aeebb0fa55f7dde04ad9af72
Alexander <fortuna12_12@mail.ru>
2025-02-10 01:32:05 +0700

    fix: перевел поля created_at и updated_at в таймзону UTC для более простой коммуникации с фронтендом

--- app/utils/base.py
+++ app/utils/base.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from sqlalchemy import func
+from sqlalchemy import func, TIMESTAMP
 from typing import Annotated
 from sqlalchemy.orm import mapped_column
 
 int_pk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
-created_at = Annotated[datetime, mapped_column(server_default=func.now(), nullable=False)]
-updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=func.now(), nullable=False)]
+created_at = Annotated[datetime, mapped_column(TIMESTAMP(timezone=True),server_default=func.now(), nullable=False)]
+updated_at = Annotated[datetime, mapped_column(TIMESTAMP(timezone=True),server_default=func.now(), onupdate=func.now(), nullable=False)]
 str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
 str_null_true = Annotated[str, mapped_column(nullable=True)]
