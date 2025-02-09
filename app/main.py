ac18a510de3822f4a621e64128b1e3bc80da8a1d
Alexander <fortuna12_12@mail.ru>
2025-02-09 18:11:55 +0700

    feat: подключил роутеры с цветами

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Depends
 
 from app.middleware.middleware import DBSessionMiddleware
-from app.routers import auth_route
-from app.routers import flower_route
+from app.routers import *
 from app.utils.user import get_current_user
 
 app = FastAPI()
@@ -21,5 +20,7 @@ async def protected_route(current_user_id: str = Depends(get_current_user)):
 
 PROTECTED = [Depends(get_current_user)]
 
-app.include_router(auth_route, tags=["auth"], prefix="/auth")
-app.include_router(flower_route, tags=["flowers"], prefix="/flowers")
+app.include_router(auth_router, tags=["auth"], prefix="/auth")
+app.include_router(flower_router, tags=["flowers"], prefix="/flowers")
+app.include_router(user_router, tags=["users"], prefix="/users")
+app.include_router(personal_flower_router, tags=["personal_flowers"], prefix="/personal_flowers")
