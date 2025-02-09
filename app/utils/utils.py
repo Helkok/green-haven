4b6c3082e7f92625318a24e13019766c043a6c55
Alexander <fortuna12_12@mail.ru>
2025-02-09 18:12:23 +0700

    fix: поменял название атрибута проверки пароля

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -9,12 +9,10 @@ from app.core.config import settings
 def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
+    return hashed_password.decode()
 
-
-def verify_password(hashed_password: str, provided_password: str) -> bool:
-    return bcrypt.checkpw(provided_password.encode('utf-8'), hashed_password.encode('utf-8'))
-
+def verify_password(provided_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(provided_password.encode(), hashed_password.encode())
 
 def create_access_token(data: dict) -> str:
     """Создает access token с информацией о пользователе."""
@@ -31,12 +29,7 @@ def verify_access_token(token: str):
     """
     try:
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
-        user_id = payload.get("sub")
-        print(user_id)
 
-        if user_id is None:
-            raise JWTError("User ID is missing from the token.")
-
-        return user_id
+        return payload
     except JWTError as e:
         return None
