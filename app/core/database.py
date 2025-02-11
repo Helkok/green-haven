7d5108d4b3999746cf9144f4e31b6f1f86a35d7f
Alexander <fortuna12_12@mail.ru>
2025-02-11 21:33:49 +0700

    test merge

--- app/core/database.py
+++ app/core/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker,AsyncSession, AsyncAttrs
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped
 from app.core.config import settings
 from app.utils.base import created_at, updated_at, int_pk
@@ -6,6 +6,7 @@ from app.utils.base import created_at, updated_at, int_pk
 engine = create_async_engine(settings.DATABASE_URL, future=True, echo=False)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False, )
 DATABASE_URL = settings.DATABASE_URL
+
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
